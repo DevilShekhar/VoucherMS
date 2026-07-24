aa97e4d7eb0e593a9d61fe79ff2f966ca6cdb992
DevilShekhar <shekhar@eternalhightech.com>
2026-07-24 12:52:12 +0530

    fav icon added and title added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Admin') }}</title>
-
+    <title>Certification Voucher Management System</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="{{ asset('assets/images/fav-icon.png') }}"  >
+    <link rel="shortcut icon" href="{{ asset('assets/images/fav-icon.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('assets/images/fav-icon.png') }}">
     <!-- Fonts & Icons -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
