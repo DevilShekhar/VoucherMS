b6f5a99785252fd46f42c0899d82816bd9020bb0
DevilShekhar <shekhar@eternalhightech.com>
2026-08-01 12:46:01 +0530

    migreation updated

--- database/migrations/2026_08_01_121255_add_created_by_and_updated_by_to_course_categories_table.php
+++ database/migrations/2026_08_01_121255_add_created_by_and_updated_by_to_course_categories_table.php
@@ -9,22 +9,27 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('course_category', function (Blueprint $table) {
-            $table->foreign('created_by')
+
+            $table->foreign('created_by', 'fk_course_category_created_by')
                 ->references('id')
                 ->on('users')
                 ->nullOnDelete();
 
-            $table->foreign('updated_by')
+            $table->foreign('updated_by', 'fk_course_category_updated_by')
                 ->references('id')
                 ->on('users')
                 ->nullOnDelete();
+
         });
     }
+
     public function down(): void
     {
         Schema::table('course_category', function (Blueprint $table) {
-            $table->dropForeign(['created_by']);
-            $table->dropForeign(['updated_by']);
+
+            $table->dropForeign('fk_course_category_created_by');
+            $table->dropForeign('fk_course_category_updated_by');
+
         });
     }
 };
