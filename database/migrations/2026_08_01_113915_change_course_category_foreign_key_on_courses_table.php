f5d870268ef1e6ee4cb4dd84ca836ae3352b83bf
Abhishek-10hue <ehtabhishek@eternalhightech.com>
2026-08-01 11:40:13 +0530

    chagned foreign key name

---
+++ database/migrations/2026_08_01_113915_change_course_category_foreign_key_on_courses_table.php
@@ -0,0 +1,31 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->dropForeign(['course_category_id']);
+
+            $table->foreign('course_category_id')
+                ->references('id')
+                ->on('course_category')
+                ->cascadeOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->dropForeign(['course_category_id']);
+
+            $table->foreign('course_category_id')
+                ->references('id')
+                ->on('course_category')
+                ->cascadeOnDelete();
+        });
+    }
+};
